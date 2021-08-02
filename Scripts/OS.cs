6cb7dcbea0d4d4b9694798b795b07d96b6a626f6
mason457 <88136451+mason457@users.noreply.github.com>
2021-08-02 18:56:23 +0800

    Add files via upload

---
+++ Scripts/OS.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OS : MonoBehaviour
+{
+    public GameObject USERLocate;
+    public GameObject LOOK;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LOOK = USERLocate;
+    }
+}
